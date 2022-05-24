4877fe95d119d123838623d8aa79dccf534ab1ea
IAmPara0x <confusedstudent13@gmail.com>
2022-05-24 18:41:17 +0530

    add These

---
+++ plutarch-extra/Plutarch/Extra/These.hs
@@ -0,0 +1,13 @@
+module Plutarch.Extra.These (PThese (..)) where
+
+import Plutarch.Prelude
+import qualified GHC.Generics as GHC
+import qualified Generics.SOP as SOP
+
+-- | Plutus These type with Scott-encoded representation.
+data PThese (a :: PType) (b :: PType) (s :: S)
+  = PThis (Term s a)
+  | PThat (Term s b)
+  | PThese (Term s a) (Term s b)
+  deriving stock (GHC.Generic)
+  deriving anyclass (SOP.Generic, PlutusType)
